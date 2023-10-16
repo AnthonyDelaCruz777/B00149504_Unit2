f83f8a68c27e9000554bf029185a23f3645cbe75
AnthonyDelaCruz777 <b00149504@mytudublin.ie>
2023-10-16 22:01:47 +0100

    This is starting to hurt

--- Unit2 Submission_B00149504/B00149504_Challenge2/Challenge 2/Assets/Challenge 2/Scripts/GitHubLink.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class GitHubLink : MonoBehaviour {
-    public void OpenChannel(){
-        Application.OpenURL("https://github.com/AnthonyDelaCruz777/B00149504_Unit2");
-    }
-}
