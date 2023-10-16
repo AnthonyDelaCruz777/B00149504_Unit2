0668085926dbecd8760ba03f790a7fabd2e38b9a
AnthonyDelaCruz777 <b00149504@mytudublin.ie>
2023-10-16 03:38:48 +0100

    Submitted it finally

---
+++ Unit2 Submission_B00149504/B00149504_Challenge2/Challenge 2/Assets/Challenge 2/Scripts/GitHubLink.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GitHubLink : MonoBehaviour {
+    public void OpenChannel(){
+        Application.OpenURL("https://github.com/AnthonyDelaCruz777/B00149504_Unit2");
+    }
+}
